ee86d9080dd8411d7851eab86796f5f294c9650a
JCandila <candy4upsn@gmail.com>
2023-11-08 18:35:20 -0500

    Update project.py

--- project.py
+++ project.py
@@ -1,12 +1,7 @@
 """
     Project code of the Spotify Karaoke
-<<<<<<< HEAD
-   
-"""
-=======
     Jason Candila, Kumail Jafari, June Lee,  Shalom Akpakla
 """
 
 
 
->>>>>>> ee19661b0a4606411936a2aa44aa612c3cac0521
