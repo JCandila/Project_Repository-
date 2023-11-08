facd02e274ea279aeb2d7ba7ff161a017cd4fcdc
shalomakpakla <sakpakla@terpmail.umd.edu>
2023-11-08 18:45:51 -0500

    Update project.py

--- project.py
+++ project.py
@@ -2,7 +2,11 @@
     Project code of the Spotify Karaoke
     Jason Candila, Kumail Jafari, June Lee,  Shalom Akpakla
 """
+"""
+Class: Song()
+    def _init_(artist, title, lyrics, songfile)
 
+"""
 """
     Jason
 """
